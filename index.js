6f251457f651a70ec9b7ffeb32385003d594ee01
Ali Hamza <alihamza022bb@gmail.com>
2023-05-03 15:49:30 +0500

    adding images

---
+++ index.js
@@ -0,0 +1,26 @@
+// Selecting Buttons
+let btnNext = document.querySelector(".btn-next")
+let btnPrev = document.querySelector(".btn-pre")
+
+// Selecting Images
+let image1 = document.querySelector(".c1")
+let image2 = document.querySelector(".c2")
+let image3 = document.querySelector(".c3")
+
+btnNext.addEventListener("click", () => {
+    console.log("button is pressed")
+    // image1.style.opacity = "0.5"
+    if(image1.style.opacity == "1") {
+        image1.style.opacity = "0"
+        image2.style.opacity = "1"
+        image3.style.opacity = "0"
+    } else if(image2.style.opacity == "1") {
+        image1.style.opacity = "0"
+        image2.style.opacity = "0"
+        image3.style.opacity = "1"
+    } else {
+        image1.style.opacity = "1"
+        image2.style.opacity = "0"
+        image3.style.opacity = "0"
+    }
+})
